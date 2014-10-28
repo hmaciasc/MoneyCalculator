4c575752d420fa0967530b7491c1f616ec7c7bc9
usuario <usuario@10.22.143.12>
2014-10-28 13:39:10 +0000

    Model implemented Application package name corrected

--- src/Model/CurrencySet.java
+++ src/Model/CurrencySet.java
@@ -1,5 +1,27 @@
 package Model;
 
-public class CurrencySet {
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class CurrencySet implements Iterable<Currency>{
+
+    @Override
+    public Iterator<Currency> iterator() {
+        return new Iterator<Currency>() {
+
+            @Override
+            public boolean hasNext() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+
+            @Override
+            public Currency next() {
+                throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+            }
+        };
+                
+    }
+    
     
 }
--- src/Model/Exchange.java
+++ src/Model/Exchange.java
@@ -1,5 +1,22 @@
 package Model;
 
 public class Exchange {
+    private final Money money;
+    private final Currency currency;
+
+    public Exchange(Money money, Currency currency) {
+        this.money = money;
+        this.currency = currency;
+    }
+
+    public Money getMoney() {
+        return money;
+    }
+
+    public Currency getCurrency() {
+        return currency;
+    }
+    
+    
     
 }
